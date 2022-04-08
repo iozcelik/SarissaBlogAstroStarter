cc8fd0892a7909ffe819ee199ad9fd25881dc36a
İsmail Özçelik <ismail.ozcelik@hayenteknoloji.com>
2022-04-08 15:20:56 +0300

    draft bug fixed

--- src/pages/searchindex.json.js
+++ src/pages/searchindex.json.js
@@ -2,15 +2,17 @@ export async function get() {
   let posts = await import.meta.globEager(`./post/*.md`);
   return {
     body: JSON.stringify(
-      Object.values(posts).map((p) => {
-        return {
-          title: p.frontmatter.title,
-          description: p.frontmatter.description,
-          date: p.frontmatter.date,
-          slug: p.url,
-          image: p.frontmatter.image,
-        };
-      }),
+      Object.values(posts)
+        .filter((f) => f.frontmatter.draft != true)
+        .map((p) => {
+          return {
+            title: p.frontmatter.title,
+            description: p.frontmatter.description,
+            date: p.frontmatter.date,
+            slug: p.url,
+            image: p.frontmatter.image,
+          };
+        }),
       false,
       1
     ),
